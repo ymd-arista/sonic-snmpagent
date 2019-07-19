0fe27b1a2056b5d0ccd0eb6e8ae25dfa3e3d6f5d
Sumukha Tumkur Vani <sumukhatv@outlook.com>
2019-07-19 11:27:40 -0700

    Add mgmt port speed test (#107)

--- tests/test_hc_interfaces.py
+++ tests/test_hc_interfaces.py
@@ -203,6 +203,25 @@ class TestGetNextPDU(TestCase):
         self.assertEqual(str(value0.name), str(ObjectIdentifier(11, 0, 1, 0, (1, 3, 6, 1, 2, 1, 31, 1, 1, 1, 1, 10001))))
         self.assertEqual(str(value0.data), 'mgmt1')
 
+    def test_mgmt_iface_speed(self):
+        """
+        Test that mgmt port speed is 1000
+        """
+        oid = ObjectIdentifier(11, 0, 0, 0, (1, 3, 6, 1, 2, 1, 31, 1, 1, 1, 15, 10000))
+        get_pdu = GetPDU(
+            header=PDUHeader(1, PduTypes.GET, 16, 0, 42, 0, 0, 0),
+            oids=[oid]
+        )
+
+        encoded = get_pdu.encode()
+        response = get_pdu.make_response(self.lut)
+        print(response)
+
+        value0 = response.values[0]
+        self.assertEqual(value0.type_, ValueType.GAUGE_32)
+        self.assertEqual(str(value0.name), str(ObjectIdentifier(11, 0, 1, 0, (1, 3, 6, 1, 2, 1, 31, 1, 1, 1, 15, 10000))))
+        self.assertEqual(value0.data, 1000)
+
     def test_in_octets(self):
         """
         For a port with no speed in the db the result should be 0
