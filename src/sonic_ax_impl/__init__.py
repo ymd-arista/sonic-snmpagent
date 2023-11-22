3b6a4ad338adc0044331b70fc1e6cd589f152829
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2023-11-22 13:27:52 +0800

    Enable faulthandler to provide more fault information (#301)
    Enable faulthandler to provide more fault information.
    
    #### Work item tracking
    Microsoft ADO (number only): 17637258
    
    **- What I did**
    Enable faulthandler to provide more fault information.
    
    **- How I did it**
    Enable faulthandler when snmp-subagent startup.
    
    **- How to verify it**
    Pass all UT
    
    **- Description for the changelog**
    Enable faulthandler to provide more fault information.

--- src/sonic_ax_impl/__init__.py
+++ src/sonic_ax_impl/__init__.py
@@ -1,9 +1,13 @@
 import json
 import logging.handlers
-
+import faulthandler
 
 # configure logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.NullHandler())
 
+# enable faulthandler to provide more information for debug
+# after enable faulthandler, the file 'stderr' will be remembered by faulthandler:
+# https://docs.python.org/dev/library/faulthandler.html#issue-with-file-descriptors
+faulthandler.enable()
